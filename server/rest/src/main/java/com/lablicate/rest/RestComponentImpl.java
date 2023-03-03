5f39552a4f29967e2256f2b372acfb1d1665ea78
Viet Mai Hoang <hviet.0906@gmail.com>
2023-03-03 10:38:29 +0700

    Staging...

--- server/rest/src/main/java/com/lablicate/rest/RestComponentImpl.java
+++ server/rest/src/main/java/com/lablicate/rest/RestComponentImpl.java
@@ -1,14 +1,24 @@
 package com.lablicate.rest;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Collection;
 import java.util.stream.Collectors;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.eclipse.chemclipse.logging.core.Logger;
 import org.osgi.service.component.annotations.Component;
@@ -47,4 +57,31 @@ public class RestComponentImpl {
 		logger.info("hello world");
 		return pInterface.getName() + " " + tcInterface.getName() + " " + logger.getClass().getName();
 	}
+
+	@POST
+	@Path("modify/upload")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response upload(@Context HttpServletRequest request) throws IOException, ServletException {
+
+		Collection<Part> parts = request.getParts();
+		Part part = request.getPart("file");
+		if (part != null && part.getSubmittedFileName() != null && part.getSubmittedFileName().length() > 0) {
+
+			StringBuilder inputBuilder = new StringBuilder();
+			try (InputStream is = part.getInputStream();
+					BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+
+				String line;
+				while ((line = br.readLine()) != null) {
+					inputBuilder.append(line).append("\n");
+				}
+			}
+
+			return Response.ok(String.join("aaaaaa\n\n")).build();
+		}
+
+		return Response.status(Status.PRECONDITION_FAILED).build();
+	}
+
 }
